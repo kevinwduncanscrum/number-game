09973c33d5c77f729752db2c9e3f3b2e2a3a94ad
kevinwduncanscrum <kevinwduncan.scrum@gmail.com>
2016-12-24 08:55:07 -0500

    Update number_game.py

--- number_game.py
+++ number_game.py
@@ -26,6 +26,34 @@ def game():
                 print("You got it! My number was {}".format(secret_num))
                 break
 
+            #Print too low or too high messages for bad guesses. I removed "#This would help me to:
+#Catch exceptions
+#Use available tools with "Imports" like the 'Random Library'.
+
+
+
+import random #must use the "import" keyword and then the name of the library
+
+
+def game():
+    #generate a random number from 1 -10
+    secret_num = random.randint(1, 10)
+    guesses = []
+    
+    while len(guesses) < 5:#when guess are 5 or greater; game will end
+                           #Limit the number of guesses
+        try:
+            #get a number guess from player
+            guess = int(input("Guess a number between 1 and 10: "))
+        except ValueError:  #Catch when someone submits a non integer
+            print("{} isn't a number!".format(guess))
+
+        else:#this is the actual guessing game
+            #compare guess to secret number
+            if guess == secret_num:
+                print("You got it! My number was {}".format(secret_num))
+                break
+
             #Print too low or too high messages for bad guesses
             elif guess < secret_num:
                 print("My number is higher than {}".format(guess))
@@ -55,3 +83,6 @@ game()#this will run the function
 
     #if guess < secret_num:
         #print ("Too Low")
+
+            
+           
